42e74496259b6d5121dc199af43ee17ffe2f9c78
Tamas Kenez <tamas.kenez@consultant.LEGO.com>
2025-08-23 00:24:25 +0200

    Add more arrays

--- src/meadow/array.h
+++ src/meadow/array.h
@@ -3,4 +3,8 @@
 #include <array>
 
 using AF2 = std::array<float, 2>;
+using AF3 = std::array<float, 3>;
+using AF4 = std::array<float, 4>;
 using AI2 = std::array<int, 2>;
+using AI3 = std::array<int, 3>;
+using AI4 = std::array<int, 4>;
