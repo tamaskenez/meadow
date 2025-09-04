5515a2879aabfe1b271d05aa0f108c8f15d76e6d
Tamas Kenez <tamas.kenez@consultant.LEGO.com>
2025-09-05 00:13:13 +0200

    Make physics/integrator to work with glm::vec

--- src/meadow/physics.h
+++ src/meadow/physics.h
@@ -10,6 +10,15 @@ enum class NewtonDynamicsIntegrator {
     runge_kutta_4
 };
 
+namespace detail
+{
+template<class T>
+T add_to_itself(const T& x)
+{
+    return x + x;
+}
+} // namespace detail
+
 // Return next position and velocity.
 template<class T, class Duration, class AccelerationFn>
 std::pair<T, T> integrate_newton_dynamics(
@@ -31,16 +40,16 @@ std::pair<T, T> integrate_newton_dynamics(
     } break;
     case NewtonDynamicsIntegrator::velocity_verlet: {
         const T a = acceleration_fn(pos, v);
-        const T next_pos = pos + v * dt + a * dt * dt / 2;
+        const T next_pos = pos + (v + a * (dt / 2)) * dt;
         const T next_a = acceleration_fn(next_pos, v);
-        const T next_v = v + (a + next_a) / 2 * dt;
+        const T next_v = v + (a + next_a) * (dt / 2);
         return std::pair(next_pos, next_v);
     } break;
     case NewtonDynamicsIntegrator::runge_kutta_2: {
         const auto k1x = v;
         const auto k1v = acceleration_fn(pos, v);
-        const auto k2x = v + k1v * dt / 2;
-        const auto k2v = acceleration_fn(pos + k1x * dt / 2, v + k1v * dt / 2);
+        const auto k2x = v + k1v * (dt / 2);
+        const auto k2v = acceleration_fn(pos + k1x * (dt / 2), v + k1v * (dt / 2));
         const auto next_pos = pos + k2x * dt;
         const auto next_v = v + k2v * dt;
         return std::pair(next_pos, next_v);
@@ -48,14 +57,14 @@ std::pair<T, T> integrate_newton_dynamics(
     case NewtonDynamicsIntegrator::runge_kutta_4: {
         const auto k1x = v;
         const auto k1v = acceleration_fn(pos, v);
-        const auto k2x = v + k1v * dt / 2;
-        const auto k2v = acceleration_fn(pos + k1x * dt / 2, v + k1v * dt / 2);
-        const auto k3x = v + k2v * dt / 2;
-        const auto k3v = acceleration_fn(pos + k2x * dt / 2, v + k2v * dt / 2);
+        const auto k2x = v + k1v * (dt / 2);
+        const auto k2v = acceleration_fn(pos + k1x * (dt / 2), v + k1v * (dt / 2));
+        const auto k3x = v + k2v * (dt / 2);
+        const auto k3v = acceleration_fn(pos + k2x * (dt / 2), v + k2v * (dt / 2));
         const auto k4x = v + k3v * dt;
         const auto k4v = acceleration_fn(pos + k3x * dt, v + k3v * dt);
-        const auto next_pos = pos + (k1x + 2 * k2x + 2 * k3x + k4x) * dt / 6;
-        const auto next_v = v + (k1v + 2 * k2v + 2 * k3v + k4v) * dt / 6;
+        const auto next_pos = pos + (k1x + detail::add_to_itself(k2x + k3x) + k4x) * (dt / 6);
+        const auto next_v = v + (k1v + detail::add_to_itself(k2v + k3v) + k4v) * (dt / 6);
         return std::pair(next_pos, next_v);
     } break;
     }
