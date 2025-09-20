e8b765bee7f8139e02200b7b954fb73f47aeb13b
Tamas Kenez <tamas.kenez@consultant.LEGO.com>
2025-09-20 18:06:46 +0200

    Add unique_token comment

--- src/meadow/unique_token.h
+++ src/meadow/unique_token.h
@@ -3,6 +3,25 @@
 // Simple, non-copyable, movable class which is either in default state (valid) or moved out (valid() == false).
 // Can be used as an internal member, for example in a class representing a resource where it saves writing the
 // copy/move/ctor/assignment functions.
+//
+// Example:
+//
+//     class SomeResource {
+//         int resource_id;
+//         unique_token token;
+//         explicit SomeResource(int resource_id_arg): resource_id(resource_id_arg) {}
+//     public:
+//         // In move-ctor and move-assignments, resource_id will be copied but token will be moved,
+//         // default implementation is fine.
+//         SomeResource(MixIsInitialized&&) = default;
+//         SomeResource& operator=(MixIsInitialized&&) = default;
+//
+//         ~SomeResource() {
+//             if (token) {
+//                 ... release the resource ...
+//             }
+//         }
+//     };
 class unique_token
 {
 public:
