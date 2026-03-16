cb8d791182d7d40d726aa781b65930f59ad7e4e7
Tamas Kenez <tamas.kenez@gmail.com>
2026-03-16 09:21:10 +0100

    Remove unnecessary inline.

--- src/meadow/include/meadow/music.h
+++ src/meadow/include/meadow/music.h
@@ -5,28 +5,28 @@
 
 template<class T>
     requires std::is_floating_point_v<T>
-inline T ratio2semitones(T ratio)
+T ratio2semitones(T ratio)
 {
     return 12 * log2(ratio);
 }
 
 template<class T>
     requires std::is_floating_point_v<T>
-inline T semitones2ratio(T semitones)
+T semitones2ratio(T semitones)
 {
     return pow(T(2), semitones / 12);
 }
 
 template<class T>
     requires std::is_floating_point_v<T>
-inline T hz2midi(T hz)
+T hz2midi(T hz)
 {
     return 69 + ratio2semitones(hz / 440);
 }
 
 template<class T>
     requires std::is_floating_point_v<T>
-inline T midi2hz(T midi)
+T midi2hz(T midi)
 {
     return 440 * semitones2ratio(midi - 69);
 }
