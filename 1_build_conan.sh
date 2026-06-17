c54888cbca9fe5153e8ae9281a6097737a3f6209
Tamas Kenez <tamas.kenez@gmail.com>
2026-06-17 23:16:47 +0200

    CI: cache conan deps

--- 1_build_conan.sh
+++ 1_build_conan.sh
@@ -1,9 +1,21 @@
 #!/bin/bash -e
 
+use_deployer=0
+for arg in "$@"; do
+	if [[ "$arg" == "--use-deployer" ]]; then
+		use_deployer=1
+	fi
+done
+
 if [[ $(uname -s) == "Darwin" ]]; then
 	cppstd=gnu23
 else
 	cppstd=23
+fi
+
+deployer_flags=()
+if [[ $use_deployer -eq 1 ]]; then
+	deployer_flags=(--deployer=full_deploy --deployer-folder=id)
 fi
 
 for build_type in Debug Release; do
@@ -11,6 +23,7 @@ for build_type in Debug Release; do
 		-b missing \
 		-pr:b default \
 		-of id/cmake \
+		"${deployer_flags[@]}" \
 		-s build_type=${build_type} \
 		-s compiler.cppstd=${cppstd}
 done
